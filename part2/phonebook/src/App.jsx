99b4db16ecf0c2e4721e62f54eeb4ca55d61bd50
Javier Velasco <Jvlsc@proton.me>
2025-03-24 18:42:40 +0100

    feat: Added Exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,26 +2,40 @@ import { useState } from 'react'
 
 // App Component:
 const App = () => {
-  
+
   // State Variables: 
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]) 
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-1234567' }]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   // Event Handler - Add Contact:
   const addPerson = (event) => {
     event.preventDefault()
     console.log('Add Contact Button Clicked', event.target)
     
-    // Check if name already exists
+    // Check if name is empty:
+    // (Additional Feature)
+    if (newName === '') {
+      alert('Name is required')
+      return
+    }
+
+    // Check if name already exists:
     if (persons.some(person => person.name === newName)) {
       alert(`${newName} is already added to phonebook`)
       return
     }
 
     // Add New Person to Phonebook:
-    const personObject = { name: newName }
+    const personObject = { 
+      name: newName,
+      number: newNumber
+    }
+
+    // Update Phonebook and Reset Form:
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   // Event Handler - Handle Name Change:
@@ -30,20 +44,35 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  // Event Handler - Handle Number Change:
+  // Check if number is valid: Only numbers and dashes (Additional Feature).
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    if (event.target.value === '' || /^[0-9-]*$/.test(event.target.value)) {
+      setNewNumber(event.target.value);
+    } 
+  }
+
   return (
     <div>
       <h2>Phonebook:</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNameChange} />
+          · Name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          · Number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
+
+      <br />
+
       <h2>Numbers:</h2>
       <ul>
-        {persons.map(person => <li key={person.name}>{person.name}</li>)}
+        {persons.map(person => <li key={person.name}>{person.name}: {person.number}</li>)}
       </ul>
     </div>
   )
