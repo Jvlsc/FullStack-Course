cab587c5b7fdeb84a7698554e94d8f03ddb641e5
Javier Velasco <Jvlsc@proton.me>
2025-03-31 16:41:35 +0200

    feat: Added Exercise 5.7

--- part5/bloglist-frontend/src/components/Blogs.jsx
+++ part5/bloglist-frontend/src/components/Blogs.jsx
@@ -1,9 +1,44 @@
+// Import Modules:
+import { useState } from 'react'
+
 // Blog Component:
-const Blog = ({ blog }) => (
-  <li>
-    {blog.title} - {blog.author}
-  </li>  
-)
+const Blog = ({ blog }) => {
+  // State Variables:
+  const [visible, setVisible] = useState(false)
+
+  // Hide/Show Visibility:
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { 
+    display: visible ? '' : 'none',
+    marginTop: '1rem',
+    marginBottom: '1rem'
+  }
+
+  // Toggle Visibility:
+  const toggleVisibility = () => setVisible(!visible)
+
+  // Render:
+  return (
+    <>
+      <li style={hideWhenVisible}>
+        {blog.title} - {blog.author} &nbsp;
+        <button onClick={toggleVisibility}>Show</button>
+      </li>
+      <li style={showWhenVisible}>
+        {blog.title} &nbsp;
+        <button onClick={toggleVisibility}>Hide</button>
+        <ul>
+          <li>{blog.author}</li>
+          <li>{blog.url}</li>
+          <li>
+            {blog.likes} &nbsp;
+            <button>Like</button>
+          </li>
+        </ul>
+      </li>
+    </>
+  )
+}
 
 // Blogs Component:
 const Blogs = ({ blogs }) => (
