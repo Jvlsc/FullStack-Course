b6bad432bec841a43cdb6cda9b1cf6d0a890db40
Javier Velasco <Jvlsc@proton.me>
2025-04-02 11:59:44 +0200

    feat: Added Exercise 6.2

--- part6/unicafe-redux/src/main.jsx
+++ part6/unicafe-redux/src/main.jsx
@@ -1,36 +1,44 @@
+// Imports Modules:
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+// Imports Redux:
 import { createStore } from 'redux'
 import reducer from './reducer'
 
+// Create Store:
 const store = createStore(reducer)
 
+// App Component:
 const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
-  }
+  // Actions:
+  const good = () => store.dispatch({ type: 'GOOD' })
+  const ok = () => store.dispatch({ type: 'OK' })
+  const bad = () => store.dispatch({ type: 'BAD' })
+  const reset = () => store.dispatch({ type: 'ZERO' })
 
+  // Render:
   return (
     <div>
       <button onClick={good}>good</button> 
-      <button>ok</button> 
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={ok}>ok</button> 
+      <button onClick={bad}>bad</button>
+      <button onClick={reset}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
 
+// Root Component:
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-const renderApp = () => {
-  root.render(<App />)
-}
+// Render App Function:
+const renderApp = () => root.render(<App />)
 
+// Render App (Initial):
 renderApp()
+
+// Subscribe to Store:
 store.subscribe(renderApp)
