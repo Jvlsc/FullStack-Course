6c5039af9320f1980954f10df2b166e5524c0a91
Javier Velasco <Jvlsc@proton.me>
2025-04-03 00:25:16 +0200

    feat: Added Exercise 6.21

--- part6/anecdotes-query/src/components/AnecdoteForm.jsx
+++ part6/anecdotes-query/src/components/AnecdoteForm.jsx
@@ -1,10 +1,27 @@
+// Import React Query Tools:
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
+// Import Services:
+import anecdotesService from '../services/anecdotes'
+
 const AnecdoteForm = () => {
+
+  const queryClient = useQueryClient()
+
+  const newAnecdoteMutation = useMutation({
+    mutationFn: anecdotesService.create,
+    onSuccess: (anecdote) => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+      queryClient.setQueryData(['anecdotes'], (anecdotes) => [...anecdotes, anecdote])
+    }
+  })
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
     console.log('new anecdote')
+    newAnecdoteMutation.mutate(content)
 }
 
   return (
