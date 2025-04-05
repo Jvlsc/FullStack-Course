823221b92d13f9322b1dc498f7b973383671c05b
Javier Velasco <Jvlsc@proton.me>
2025-04-05 20:28:42 +0200

    Revert "Revert "Part7 redux" Merge" (#4)

--- part7/bloglist-ultimate/src/App.jsx
+++ part7/bloglist-ultimate/src/App.jsx
@@ -1,5 +1,12 @@
-// Import Hooks and Services:
-import { useState, useEffect, useRef } from 'react'
+// Import React Hooks:
+import { useEffect, useRef } from 'react'
+
+// Import Redux Hooks:
+import { useSelector, useDispatch } from 'react-redux'
+
+// Import Reducer Functions:
+import { getAllBlogs } from './reducers/blogsReducer'
+import { setSession, clearSession } from './reducers/sessionReducer'
 
 // Import Components:
 import LoginForm from './components/LoginForm'
@@ -11,153 +18,50 @@ import Togglable from './components/Togglable'
 
 // Import Services:
 import blogService from './services/blogs'
-import loginService from './services/login'
 
 // App Component:
 const App = () => {
-  // State Variables:
-  const [blogs, setBlogs] = useState([])
-  const [user, setUser] = useState(null)
+  const user = useSelector((state) => state.session.username)
+  const dispatch = useDispatch()
 
-  // Refs:
   const blogFormRef = useRef()
-  const notificationRef = useRef()
 
-  // Helper function for notifications
-  const showNotification = (message, type) => {
-    notificationRef.current.showNotification(message, type)
-  }
-
-  // Effect Hook - Check User Session:
   useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('login')
-    if (loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON)
+    console.log('[App Component] Checking user session...')
+    const userJSON = window.localStorage.getItem('login')
+    if (userJSON) {
+      const user = JSON.parse(userJSON)
+      console.log('[App Component] User session found:', user.username)
       blogService.setToken(user.token)
-      setUser(user)
+      dispatch(setSession(user))
+      dispatch(getAllBlogs())
+    } else {
+      console.log('[App Component] No user session found.')
+      dispatch(clearSession())
     }
-  }, [])
+  }, [user, dispatch])
 
-  // Effect Hook - Get All Blogs:
-  useEffect(() => {
-    blogService.getAll().then((blogs) => setBlogs(blogs))
-  }, [])
-
-  // Login Handler:
-  const handleLogin = async (userObject) => {
-    try {
-      console.log('Logging in...')
-      const user = await loginService.login(userObject)
-
-      console.log('User logged in:', user)
-      window.localStorage.setItem('login', JSON.stringify(user))
-      blogService.setToken(user.token)
-      setUser(user)
-
-      showNotification(`User '${userObject.username}' logged in successfully!`, 'success')
-    } catch (exception) {
-      const errorMessage = exception.response.data.error
-      console.error(errorMessage)
-      showNotification(`User '${userObject.username}' login failed! ${errorMessage}`, 'error')
-    }
+  if (user === null || user === undefined) {
+    return (
+      <div>
+        <h2>Login:</h2>
+        <Notification />
+        <LoginForm />
+      </div>
+    )
   }
 
-  // Logout Handler:
-  const handleLogout = () => {
-    window.localStorage.removeItem('login')
-    setUser(null)
-    showNotification('User logged out successfully!', 'success')
-  }
-
-  // Create Blog Handler:
-  const handleCreate = async (blogObject) => {
-    try {
-      console.log('Creating blog...')
-      const newBlog = await blogService.create(blogObject)
-
-      console.log('Blog created:', newBlog)
-
-      // Fix populate mismatch:
-      const user = JSON.parse(window.localStorage.getItem('login'))
-      newBlog.user = {
-        id: newBlog.user,
-        name: user.name,
-        username: user.username,
-      }
-      setBlogs(blogs.concat(newBlog))
-
-      blogFormRef.current.toggleVisibility()
-      showNotification(`Blog '${newBlog.title}' created successfully!`, 'success')
-    } catch (exception) {
-      const errorMessage = exception.response.data.error
-      console.error(errorMessage)
-      showNotification(`Blog '${blogObject.title}' creation failed! ${errorMessage}`, 'error')
-    }
-  }
-
-  // Update Blog Handler:
-  const handleUpdate = async (blog) => {
-    try {
-      console.log('Updating blog...')
-      const updatedBlog = await blogService.update(blog.id, { likes: blog.likes + 1 })
-
-      setBlogs(blogs.map((blog) => (blog.id === updatedBlog.id ? { ...blog, likes: updatedBlog.likes } : blog)))
-
-      showNotification(`Blog '${updatedBlog.title}' updated successfully!`, 'success')
-    } catch (exception) {
-      const errorMessage = exception.response.data.error
-      console.error(errorMessage)
-      showNotification(`Blog '${blog.title}' update failed! ${errorMessage}`, 'error')
-    }
-  }
-
-  // Delete Blog Handler:
-  const handleDelete = async (id) => {
-    const blogToDelete = blogs.find((blog) => blog.id === id)
-    if (!window.confirm(`Are you sure you want to delete "${blogToDelete.title}" blog?`)) {
-      return
-    }
-
-    try {
-      console.log('Deleting blog...')
-      await blogService.remove(id)
-
-      setBlogs(blogs.filter((blog) => blog.id !== id))
-
-      showNotification(`Blog '${blogToDelete.title}' deleted successfully!`, 'success')
-    } catch (exception) {
-      const errorMessage = exception.response.data.error
-      console.error(errorMessage)
-      showNotification(`Blog '${blogToDelete.title}' deletion failed! ${errorMessage}`, 'error')
-    }
-  }
-
-  // Sort Blogs by Likes:
-  const sortedBlogs = blogs.sort((a, b) => b.likes - a.likes)
-  console.log('Sorted blogs:', sortedBlogs)
-
-  // Render:
   return (
     <div>
-      {user === null ? (
-        <>
-          <h2>Login:</h2>
-          <Notification ref={notificationRef} />
-          <LoginForm handleLogin={handleLogin} />
-        </>
-      ) : (
-        <>
-          <h2>Blogs:</h2>
-          <Notification ref={notificationRef} />
-          <User user={user.name} handleLogout={handleLogout} />
-          <br />
-          <Togglable buttonLabel="Create New Blog" ref={blogFormRef}>
-            <BlogForm handleCreate={handleCreate} />
-          </Togglable>
-          <br />
-          <Blogs blogs={sortedBlogs} handleUpdate={handleUpdate} handleDelete={handleDelete} />
-        </>
-      )}
+      <h2>Blogs:</h2>
+      <Notification />
+      <User />
+      <br />
+      <Togglable buttonLabel="Create New Blog" ref={blogFormRef}>
+        <BlogForm blogFormRef={blogFormRef} />
+      </Togglable>
+      <br />
+      <Blogs />
     </div>
   )
 }
---
+++ part7/bloglist-ultimate/src/hooks/useField.js
@@ -0,0 +1,16 @@
+// Import React Hooks:
+import { useState } from 'react'
+
+// Custom Hook - useField:
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => setValue(event.target.value)
+
+  const onReset = () => setValue('')
+
+  return { type, value, onChange, onReset }
+}
+
+// Export the useField Custom Hook:
+export default useField
