d9f465f50bfc9b802334e8cb50aab38432730e6d
Javier Velasco <Jvlsc@proton.me>
2025-04-06 22:16:57 +0200

    feat: Added fontawesome and improve navbar

--- part7/bloglist-ultimate/src/components/Navigation.jsx
+++ part7/bloglist-ultimate/src/components/Navigation.jsx
@@ -5,18 +5,30 @@ import { Link } from 'react-router-dom'
 import User from './User'
 
 // Import Bootstrap Components:
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Navbar, Nav } from 'react-bootstrap'
+
+// Import Font Awesome:
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faUsers } from '@fortawesome/free-solid-svg-icons'
 
 // Navigation Component:
 const Navigation = () => {
   return (
-    <Navbar bg="light" expand="lg" className="mb-4 px-4" fluid>
-      <Navbar.Brand as={Link} to="/">BlogList</Navbar.Brand>
+    <Navbar bg="light" expand="lg" className="mb-4 px-4" fluid="true">
+      <Navbar.Brand as={Link} to="/">
+        BlogList
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link as={Link} to="/">Home</Nav.Link>
-          <Nav.Link as={Link} to="/users">Users</Nav.Link>
+          <Nav.Link as={Link} to="/">
+            <FontAwesomeIcon icon={faHome} className="me-1" />
+            Home
+          </Nav.Link>
+          <Nav.Link as={Link} to="/users">
+            <FontAwesomeIcon icon={faUsers} className="me-1" />
+            Users
+          </Nav.Link>
         </Nav>
         <Nav>
           <User />
--- part7/bloglist-ultimate/src/components/User.jsx
+++ part7/bloglist-ultimate/src/components/User.jsx
@@ -1,6 +1,16 @@
+// Import React Router:
+import { Link } from 'react-router-dom'
+
 // Import Contexts Hooks:
 import { useSessionUser, useSessionClearDispatch } from '../contexts/SessionContext'
 import { useNotificationDispatch } from '../contexts/NotificationContext'
+
+// Import Bootstrap Components:
+import { NavDropdown } from 'react-bootstrap'
+
+// Import Font Awesome:
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 
 // User Component:
 const User = () => {
@@ -14,13 +24,26 @@ const User = () => {
     notificationDispatch('User logged out successfully!', 'success')
   }
 
+  if (!user) {
+    return null
+  }
+
   return (
-    <>
-      {user?.username} logged in &nbsp;
-      <button data-testid="logout-button" onClick={handleLogout}>
+    <NavDropdown id="user-dropdown" align="end" title={
+      <span>
+        <FontAwesomeIcon icon={faUser} className="me-1" />
+        {user?.name}
+      </span>
+    }>
+      <NavDropdown.Item as={Link} to={`/users/${user.id}`}>
+        <FontAwesomeIcon icon={faUser} className="me-2" />
+        Profile
+      </NavDropdown.Item>
+      <NavDropdown.Item onClick={handleLogout} data-testid="logout-button">
+        <FontAwesomeIcon icon={faSignOutAlt} className="me-2" />
         Logout
-      </button>
-    </>
+      </NavDropdown.Item>
+    </NavDropdown>
   )
 }
 
