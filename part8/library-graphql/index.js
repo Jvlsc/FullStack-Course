69f9037869bd68d3beb5b6e2b90066c04d422dd4
Javier Velasco <Jvlsc@proton.me>
2025-04-07 17:48:20 +0200

    feat: Added Exercise 8.4

--- part8/library-graphql/index.js
+++ part8/library-graphql/index.js
@@ -96,7 +96,7 @@ const typeDefs = `
   type Query {
     bookCount: Int
     authorCount: Int
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -105,7 +105,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (args.author) {
+        return books.filter(book => book.author === args.author)
+      }
+      return books
+    },
     allAuthors: () => authors.map(author => ({
       ...author,
       bookCount: books.filter(book => book.author === author.name).length
