c8cf006bdf4ac4dac4b7549ad7e48c5d0d824fbd
Javier Velasco <Jvlsc@proton.me>
2025-04-11 17:02:47 +0200

    feat: Added Exercise 9.9

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,14 +1,24 @@
+// Import Modules:
 import express from 'express';
+import cors from 'cors';
+
+// Constants:
+const PORT = 3001;
+
+// Create Express App:
 const app = express();
+
+// Middlewares:
+app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
-
+// Routes:
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
 
+// Start Server:
 app.listen(PORT, () => {
   console.log(`Server Running: http://localhost:${PORT}`);
 });
