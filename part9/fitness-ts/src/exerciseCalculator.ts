a15e2ec0d417eae445814fc63debead1ddf9fd9b
Javier Velasco <Jvlsc@proton.me>
2025-04-11 14:01:08 +0200

    feat: Added Exercise 9.7

--- part9/fitness-ts/src/exerciseCalculator.ts
+++ part9/fitness-ts/src/exerciseCalculator.ts
@@ -1,7 +1,7 @@
 // Exercise Values Interface:
 interface ExerciseValues {
   target: number;
-  data: Array<number>;
+  daily_exercises: Array<number>;
 }
 
 // Exercise Result Interface:
@@ -19,27 +19,27 @@ interface ExerciseResult {
 const parseExerciseArguments = (args: string[]): ExerciseValues => {
   if (args.length < 4) throw new Error('Not enough arguments');
   const target = Number(args[2]);
-  const data = args.slice(3).map(Number);
+  const daily_exercises = args.slice(3).map(Number);
 
-  if (!isNaN(target) && !data.every(isNaN)) {
-    return { target, data } as ExerciseValues;
+  if (!isNaN(target) && !daily_exercises.every(isNaN)) {
+    return { target, daily_exercises } as ExerciseValues;
   } else {
     throw new Error('Provided values were not numbers!');
   }
 };
 
 // Exercise Calculator:
-const calculateExercises = (target: number, data: Array<number>): ExerciseResult => {
-  const periodLength = data.length;
-  const trainingDays = data.filter(day => day !== 0).length;
-  const average = data.reduce((acc, curr) => acc + curr, 0) / data.length;
+const calculateExercises = (target: number, daily_exercises: Array<number>): ExerciseResult => {
+  const periodLength = daily_exercises.length;
+  const trainingDays = daily_exercises.filter(day => day !== 0).length;
+  const average = daily_exercises.reduce((acc, curr) => acc + curr, 0) / daily_exercises.length;
   const success = average >= target;
   const rating = 
     average >= target ? 3 : 
     average >= target * 0.8 ? 2 : 1;
   const ratingDescription = 
     rating === 3 ? 'Excellent job!' : 
-    rating === 2 ? 'Not too bad but could be better' : 'You need to work harder';
+    rating === 2 ? 'Not too bad but could be better' : 'Really bad!';
 
   return {
     periodLength,
@@ -52,13 +52,17 @@ const calculateExercises = (target: number, data: Array<number>): ExerciseResult
   } as ExerciseResult;
 };
 
-// Run the Exercise Calculator:
-// (Hardcoded Values)
-try {
-  const { target, data } = parseExerciseArguments(process.argv);
-  console.log(calculateExercises(target, data));
-} catch (error) {
-  let errorMessage = "Error: ";
-  if (error instanceof Error) errorMessage += error.message;
-  console.log(errorMessage);
+// Export Functions:
+export { calculateExercises, ExerciseValues };
+
+// Run as a Script:
+if (require.main === module) {
+  try {
+    const { target, daily_exercises } = parseExerciseArguments(process.argv);
+    console.log(calculateExercises(target, daily_exercises));
+  } catch (error) {
+    let errorMessage = "Error: ";
+    if (error instanceof Error) errorMessage += error.message;
+    console.log(errorMessage);
+  }
 }
--- part9/fitness-ts/src/index.ts
+++ part9/fitness-ts/src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import { calculateBmi } from './bmiCalculator';
+import { calculateExercises, ExerciseValues } from './exerciseCalculator';
+
 const app = express();
 
-import { calculateBmi } from './bmiCalculator';
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
@@ -24,6 +27,25 @@ app.get('/bmi', (req, res) => {
 
   res.json({ height, weight, bmi });
 });
+
+app.post('/exercises', (req, res) => {
+  const { target, daily_exercises } = req.body as ExerciseValues;
+
+  if (!target || !daily_exercises) {
+    res.status(400).json({ error: "Missing parameters" });
+    return;
+  }
+
+  if (isNaN(Number(target)) || daily_exercises.map(Number).some(isNaN)) {
+    res.status(400).json({ error: "Invalid parameters" });
+    return;
+  }
+
+  const result = calculateExercises(Number(target), daily_exercises.map(Number));
+
+  res.json(result);
+});
+
 
 const PORT = 3003;
 
